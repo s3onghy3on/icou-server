c716b06a27c4a130880720ce5bb4c0b0106ee6c8
s3onghy3on <s3onghy3on@gmail.com>
2020-05-06 18:28:21 +0900

    ssl

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const https = require("https");
-const http = require("http");
-const greenlock = require("greenlock-express");
+// const https = require("https");
+// const http = require("http");
 
 const app = require("./app.js");
-const lex = greenlock.create({
-  version: "v02",
-  configDir: "/etc/letsencrypt",
-  server: "https://acme-v02.api.letsencrypt.org/directory",
-  email: "s3onghy3on@gmail.com",
-  approveDomains: ["atagl.com", "www.atagl.com"],
-  agreeTos: true,
-  renewWithin: 81 * 24 * 60 * 60 * 1000,
-  renewBy: 80 * 24 * 60 * 60 * 1000,
+// const lex = greenlock.create({
+//   version: "v02",
+//   configDir: "/etc/letsencrypt",
+//   server: "https://acme-v02.api.letsencrypt.org/directory",
+//   email: "s3onghy3on@gmail.com",
+//   approveDomains: ["atagl.com", "www.atagl.com"],
+//   agreeTos: true,
+//   renewWithin: 81 * 24 * 60 * 60 * 1000,
+//   renewBy: 80 * 24 * 60 * 60 * 1000,
+// });
+require("greenlock-express").init({
+  packageRoot: __dirname,
+  configDir: "./greenlock.d",
+  maintainerEmail: "s3onghy3on@gmail.com",
+  cluster: false,
 });
 
 const searchRouter = require("./routes/search.js");
@@ -24,9 +29,9 @@ app.get("/", (req, res) => {
 
 app.use("/search", searchRouter);
 
-https
-  .createServer(lex.httpsOptions, lex.middleware(app))
-  .listen(process.env.SSL_PORT || 443);
-http
-  .createServer(lex.middleware(require("redirect-https")()))
-  .listen(process.env.PORT || 80);
+// https
+//   .createServer(lex.httpsOptions, lex.middleware(app))
+//   .listen(process.env.SSL_PORT || 443);
+// http
+//   .createServer(lex.middleware(require("redirect-https")()))
+//   .listen(process.env.PORT || 80);
